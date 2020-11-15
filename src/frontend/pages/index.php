4c2acc466cc4db48872c76ae3f26415a3b37c1c3
Marcin Mizgała <maro181114@gmail.com>
2020-11-15 17:47:29 +0100

    Add login form to login page and form_container class to login.css

--- src/frontend/pages/index.php
+++ src/frontend/pages/index.php
@@ -31,6 +31,39 @@
 
 </header>
 
+<main>
+
+    <article>
+
+        <div class="container">
+
+            <div class="form_container">
+
+                <h3>Log in</h3>
+                <form action="" method="post">
+
+                    <input class="d-block mx-auto my-2 col-sm-10 col-md-6 col-lg-4 col-xl-3" type="email" placeholder="E-mail address" name="email"  autocomplete="off">
+
+                    <input class="d-block mx-auto my-2 col-sm-10 col-md-6 col-lg-4 col-xl-3" type="password" placeholder="password" name="password"  autocomplete="off">
+
+                    <input type="submit" class="d-block mx-auto my-2 col-sm-10 col-md-6 col-lg-4 col-xl-3" value="Log in">
+
+                    <label for="remember">
+
+                        <input type="checkbox" name="remember" id="remember"> Remember me
+
+                    </label>
+
+                </form>
+
+            </div>
+
+        </div>
+
+    </article>
+
+</main>
+
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
