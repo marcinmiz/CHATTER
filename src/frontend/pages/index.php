0615d4bab0f6c4ad1a2c567c014b64cc3cac9fbf
Marcin Mizgała <maro181114@gmail.com>
2020-11-15 16:21:53 +0100

    Create main stylesheet and link to login page

--- src/frontend/pages/index.php
+++ src/frontend/pages/index.php
@@ -13,6 +13,8 @@
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
+    <link rel="stylesheet" href="../stylesheets/main.css">
+
     <!--[if lt IE 9]-->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
     <!--[endif]-->
