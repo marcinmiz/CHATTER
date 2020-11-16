af706671019e50965a736476e07703e17f37818c
Marcin Mizgała <maro181114@gmail.com>
2020-11-17 00:47:50 +0100

    Create Cookie put() and delete() methods

--- src/backend/model/Cookie.php
+++ src/backend/model/Cookie.php
@@ -13,4 +13,15 @@ class Cookie
         return $_COOKIE[$name];
     }
 
+    public static function put($name, $value, $expiry) {
+        if (setcookie($name, $value, time() + $expiry, '/')) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static function delete($name) {
+        self::put($name, '', time() - 1);
+    }
 }
