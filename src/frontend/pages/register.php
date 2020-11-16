7038c93a1c3866fe13443674a66a87e305b61bce
Marcin Mizgała <maro181114@gmail.com>
2020-11-16 16:33:09 +0100

    Create register page template and link google font and html5shiv script

---
+++ src/frontend/pages/register.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>CHATTER register</title>
+    <meta name="description" content="Web chat App">
+    <meta name="keywords" content="chat, web chat">
+    <meta name="author" content="Marcin Mizgała">
+    <meta http-equiv="X-Ua-Compatible" content="IE=edge">
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&amp;subset=latin-ext" rel="stylesheet">
+    <!--[if lt IE 9]-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <!--[endif]-->
+</head>
+<body>
+
+
+
+</body>
