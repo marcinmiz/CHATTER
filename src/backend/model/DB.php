1bbb26ddc78466c511a6a98a0e2c51fb1459bc2a
Marcin Mizgała <maro181114@gmail.com>
2020-11-17 17:37:40 +0100

    Add query() method

--- src/backend/model/DB.php
+++ src/backend/model/DB.php
@@ -29,4 +29,26 @@ class DB
 
         return self::$instance;
     }
+
+    public function query($sql, $params = array()) {
+        $this->_error = false;
+        if ($this->_query = $this->_pdo->prepare($sql)) {
+            $x = 1;
+            if (count($params)) {
+                foreach($params as $param) {
+                    $this->_query->bindValue($x, $param);
+                    $x++;
+                }
+            }
+
+            if ($this->_query->execute()) {
+                $this->_results = $this->_query->fetchAll(\PDO::FETCH_OBJ);
+                $this->_count = $this->_query->rowCount();
+            } else {
+                $this->_error = true;
+            }
+        }
+
+        return $this;
+    }
 }
