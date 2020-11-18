efce45708c660b69bb832111774a788facc48669
Marcin Mizgała <maro181114@gmail.com>
2020-11-18 15:45:20 +0100

    Create activate page template, link bootstrap, main and login stylesheets

---
+++ src/frontend/pages/activate.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>CHATTER activate</title>
+    <meta name="description" content="Web chat App">
+    <meta name="keywords" content="chat, web chat">
+    <meta name="author" content="Marcin Mizgała">
+    <meta http-equiv="X-Ua-Compatible" content="IE=edge">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="../stylesheets/main.css">
+
+    <link rel="stylesheet" href="../stylesheets/login.css">
+
+</head>
+<body>
+
+</body>
+</html>
