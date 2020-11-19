43c2862bc162c3d88d74fdd6db4a470d26b5a73e
Marcin Mizgała <maro181114@gmail.com>
2020-11-19 01:19:41 +0100

    Link fontello, main and users stylesheets

--- src/frontend/pages/users.php
+++ src/frontend/pages/users.php
@@ -11,6 +11,12 @@
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
+    <link href="../../extras/fontello/css/fontello.css" rel="stylesheet" type="text/css"/>
+
+    <link rel="stylesheet" href="../stylesheets/main.css">
+
+    <link rel="stylesheet" href="../stylesheets/users.css">
+
 </head>
 <body>
 
