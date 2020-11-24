a04e9c0d4a70e28383c17671bc97c0cc9e40a807
Marcin Mizgała <maro181114@gmail.com>
2020-11-24 13:51:59 +0100

    Add getStatuses() function

--- src/frontend/scripts/users.js
+++ src/frontend/scripts/users.js
@@ -1,6 +1,10 @@
 function go(current_user_id) {
+
+    var usersNumber = 0;
+
     setInterval(function () {
-        updateLastActivity();}, 3000);
+        updateLastActivity();
+    }, 3000);
 
     function updateLastActivity(id = current_user_id) {
         fetch("../../api/users/update/last_activity/"+id+"/", {
@@ -11,5 +15,21 @@ function go(current_user_id) {
             .then(res => res.json())
             .then(data => console.log(data))
             .catch(error => console.log(error))
+    }
+
+    function getStatuses(id = current_user_id) {
+        fetch('../../api/users/get/statuses/'+id+"/", {
+            headers: {
+                'Accept': 'application/json'
+            }
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                for (let i = 0; i < usersNumber; i++) {
+                    document.getElementById('status' + data[i].user_id).innerHTML = data[i].last_activity;
+                }
+            })
+            .catch((error) => console.log(error))
     }
 }
