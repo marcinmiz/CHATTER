223f2c2b65a40c1b55d4bdea2b8db542ea3e09c6
Marcin Mizgała <maro181114@gmail.com>
2020-11-25 20:02:39 +0100

    Create chat page template, link main stylesheet and fontello

---
+++ src/frontend/pages/chat.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once '../../core/init.php';
+
+?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>CHATTER Chat</title>
+    <meta name="description" content="Web chat App">
+    <meta name="keywords" content="chat, web chat">
+    <meta name="author" content="Marcin Mizgała">
+    <meta http-equiv="X-Ua-Compatible" content="IE=edge">
+
+    <link href="../../extras/fontello/css/fontello.css" rel="stylesheet" type="text/css"/>
+
+    <link rel="stylesheet" href="../stylesheets/main.css">
+
+</head>
+<body>
+
+
+
+</body>
+</html>
