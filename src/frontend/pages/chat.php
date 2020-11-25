cc91af59b46f1abd88b5fc4e0f49423bcdbb25b8
Marcin Mizgała <maro181114@gmail.com>
2020-11-25 21:08:58 +0100

    Append getFile() function and load mainbar for sites after logon

--- src/frontend/pages/chat.php
+++ src/frontend/pages/chat.php
@@ -25,6 +25,33 @@ require_once '../../core/init.php';
 </head>
 <body>
 
+    <div id="header">Loading</div>
+
+    <?php
+    echo <<< END
+    
+    <script>
+
+        function getFile(fileName, element) {
+            
+            fetch(fileName, {
+                headers: {
+                    'Accept': 'text/html'}
+            })
+                .then(res => res.text())
+                .then((data) => {
+                    document.getElementById(element).innerHTML = data;
+                })
+                .catch((error) => console.log(error))
+        }
+        getFile("extended_mainbar.php", 'header');
+
+    </script>
+
+END;
+
+    ?>
+
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
