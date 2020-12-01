f26a34efcb73b24a32b3a0a8c02090f92d189a5b
Marcin Mizgała <maro181114@gmail.com>
2020-12-01 23:43:14 +0100

    Add test cases for successful get all new private messages operation and failed get all new group messages operation

--- tests/unit/ChatTest.php
+++ tests/unit/ChatTest.php
@@ -116,4 +116,40 @@ class ChatTest extends TestCase
         $chat = new Chat($this->dbMock);
         $this->assertEqualsCanonicalizing(false, $chat->getAllMessages($data));
     }
+
+    public function testSuccessfulGetAllNewPrivateMessages() {
+        $this->dbMock->method('error')->willReturn(false);
+        $messages = array(
+            'user_name' => 'Cindy',
+            'surname' => 'Dindi',
+            'receiver_id' => 15,
+            'message_text' => 'Hey!',
+            'sending_date' => '2020-11-30 15:33:44'
+        );
+        $this->dbMock->method('results')->willReturn($messages);
+
+        $data = [
+            'action' => 'get',
+            'complement' => 'all_new_messages',
+            'current_user_id' => 16,
+            'another_user_id' => 15,
+            'group' => false
+        ];
+        $chat = new Chat($this->dbMock);
+        $this->assertEqualsCanonicalizing($messages, $chat->getAllMessages($data));
+    }
+
+    public function testFailedGetAllNewGroupMessages() {
+        $this->dbMock->method('error')->willReturn(true);
+
+        $data = [
+            'action' => 'get',
+            'complement' => 'all_new_messages',
+            'current_user_id' => 16,
+            'another_user_id' => 15,
+            'group' => true
+        ];
+        $chat = new Chat($this->dbMock);
+        $this->assertEqualsCanonicalizing(false, $chat->getAllMessages($data));
+    }
 }
