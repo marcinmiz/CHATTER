e889689f688565ac3fcbdc5a40b0fb6a3cb8c0f5
Marcin Mizgała <maro181114@gmail.com>
2020-12-01 18:07:53 +0100

    Add Chat Rest Handler test cases for successful and failed get all messages operation, correct line separator

--- tests/unit/ChatRestHandlerTest.php
+++ tests/unit/ChatRestHandlerTest.php
@@ -12,6 +12,7 @@ class ChatRestHandlerTest extends TestCase
     {
         $this->chat = $this->getMockBuilder('backend\model\Redirect')
             ->addMethods(['sendMessage'])
+            ->addMethods(['getAllMessages'])
             ->getMock();
 
     }
@@ -53,6 +54,55 @@ class ChatRestHandlerTest extends TestCase
             'group' => true
         ];
         $this->assertEquals(404, $restHandler->sendMessage($data));
+        unset($_SERVER['HTTP_ACCEPT']);
+    }
+    /**
+     * @test
+     * @runInSeparateProcess
+     **/
+    public function testSuccessfulGetAllMessages () {
+        $_SERVER['HTTP_ACCEPT'] = 'application/json';
+
+        $data = [
+            'action' => 'get',
+            'complement' => 'all_messages',
+            'current_user_id' => 16,
+            'another_user_id' => 15,
+            'group' => false
+        ];
+
+        $messages = array(
+            'user_name' => 'Cindy',
+            'surname' => 'Dindi',
+            'receiver_id' => 15,
+            'message_text' => 'Hey!',
+            'sending_date' => '2020-11-30 15:33:44'
+        );
+
+        $this->chat->method('getAllMessages')->willReturn($messages);
+        $rest_handler = new ChatRestHandler($this->chat);
+        $this->assertEquals(200, $rest_handler->getAllMessages($data));
+        unset($_SERVER['HTTP_ACCEPT']);
+    }
+
+    /**
+     * @test
+     * @runInSeparateProcess
+     **/
+    public function testFailedGetAllMessages () {
+        $_SERVER['HTTP_ACCEPT'] = 'application/json';
+
+        $data = [
+            'action' => 'get',
+            'complement' => 'all_messages',
+            'current_user_id' => 16,
+            'another_user_id' => 15,
+            'group' => false
+        ];
+
+        $this->chat->method('getAllMessages')->willReturn(false);
+        $rest_handler = new ChatRestHandler($this->chat);
+        $this->assertEquals(404, $rest_handler->getAllMessages($data));
         unset($_SERVER['HTTP_ACCEPT']);
     }
 }
